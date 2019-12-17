bb6f3f177e3ba150c0cf2690c55eb797b3de1221
wangjian136 <1451518614@qq.com>
2019-12-17 16:56:31 +0800

    something change

--- 08-springboot-exception/src/main/java/cn/edu/ccut/controller/HelloController.java
+++ 08-springboot-exception/src/main/java/cn/edu/ccut/controller/HelloController.java
@@ -4,8 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
  
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
  
 @Controller
 public class HelloController {
@@ -21,6 +23,24 @@ public class HelloController {
 	
 	@RequestMapping("/show")
 	public String show(){
-		return "/index.html";
+		return "/index";
 	}
+	
+	/**
+	 * 使用@ExceptionHandler注解处理程序中出现的异常
+	 * 这里展示处理ArithmeticException和NullPointerException
+	 * @param e
+	 * @return
+	 */
+	/*@ExceptionHandler(value={ArithmeticException.class,NullPointerException.class})
+	public ModelAndView exceptionHandler(Exception e){
+		ModelAndView mv = new ModelAndView();
+		if(e instanceof ArithmeticException){
+			mv.setViewName("error1");
+		}else if(e instanceof NullPointerException){
+			mv.setViewName("error2");
+		}
+		mv.addObject("error", e.toString());
+		return mv;
+	}*/
 }
